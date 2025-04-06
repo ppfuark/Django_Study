d77a551cd8cbe684bb5ee8bd1d390cb717422e25
ppfuark <phfuark2007@gmail.com>
2025-04-05 23:39:57 -0300

    First Commit

---
+++ ToDoList/api/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class ToDoList(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+class Item(models.Model):
+    todolist = models.ForeignKey(ToDoList, on_delete=models.CASCADE)
