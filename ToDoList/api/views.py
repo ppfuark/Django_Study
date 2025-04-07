0dff58f292bb6959eacbafa9df61989f46faffd5
ppfuark <phfuark2007@gmail.com>
2025-04-07 16:10:17 -0300

    Simple Form - List

--- ToDoList/api/views.py
+++ ToDoList/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
+from .forms import *
 
 def index(request, id):
     todo_list = ToDoList.objects.get(id=id)
@@ -10,4 +11,13 @@ def home(request):
     return render(request, "api/home.html", {})
 
 def create(request):
-    return render(request, "api/create.html", {})
+    if request.method == "POST":
+        form = CreateNewList(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            todo_list = ToDoList(name=name) 
+            todo_list.save()
+        return HttpResponseRedirect("/%i" % todo_list.id)
+    else:    
+        form = CreateNewList()
+    return render(request, "api/create.html", {"form":form})
