01a3e4cf902a1cdda00a1ea6374470031c6d32b5
pp.fuark <phfuark2007@gmail.com>
2025-04-08 08:53:36 -0300

    ToDoList without UserAuth

--- ToDoList/api/views.py
+++ ToDoList/api/views.py
@@ -1,23 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from .models import *
-from .forms import *
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseRedirect
+from .models import ToDoList, Item
+from .forms import CreateNewList
 
 def index(request, id):
-    todo_list = ToDoList.objects.get(id=id)
+    todo_list = get_object_or_404(ToDoList, id=id)
+    
+    if request.method == "POST":
+        if request.POST.get("save"):
+            for item in todo_list.item_set.all():
+                if request.POST.get('c'+str(item.id)) == 'clicked':
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+
+        elif request.POST.get("newItem"):
+            new_item_text = request.POST.get("new")
+            if len(new_item_text) > 1:
+                todo_list.item_set.create(text=new_item_text, complete=False)
+            else:
+                new_item_text = request.POST.get("new")
+                print("Invalid:" + new_item_text)
+
     return render(request, "api/list.html", {"todo_list": todo_list})
     
 def home(request):
-    return render(request, "api/home.html", {})
+    todo_lists = ToDoList.objects.all()
+    return render(request, "api/home.html", {"todo_lists": todo_lists})
 
 def create(request):
     if request.method == "POST":
         form = CreateNewList(request.POST)
         if form.is_valid():
-            name = form.cleaned_data["name"]
-            todo_list = ToDoList(name=name) 
-            todo_list.save()
-        return HttpResponseRedirect("/%i" % todo_list.id)
+            todo_list = ToDoList.objects.create(
+                name=form.cleaned_data['name'] 
+            )
+            return redirect("list", id=todo_list.id)
     else:    
         form = CreateNewList()
-    return render(request, "api/create.html", {"form":form})
+    
+    return render(request, "api/create.html", {"form": form})
--- ToDoList/backend/settings.py
+++ ToDoList/backend/settings.py
@@ -112,13 +112,18 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configurações de arquivos estáticos
+STATIC_URL = '/static/'
+
+# Adicione essa configuração se você tem arquivos estáticos fora do diretório da aplicação
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Para arquivos estáticos no diretório raiz
+]
+
+# Diretório onde os arquivos estáticos serão armazenados após o comando collectstatic
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Só é necessário para produção
