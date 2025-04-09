4e06f996ef9a7f33c7013093c78004994b467b7f
pp.fuark <phfuark2007@gmail.com>
2025-04-09 10:58:23 -0300

    User Registration | Sing Up page

--- ToDoList/accounts/views.py
+++ ToDoList/accounts/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from . import forms
 
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm
+    if request.method == "POST":
+        form = forms.RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/")
+    else:
+        form = forms.RegisterForm()
     return render(request, "accounts/register.html", {"form":form})
--- ToDoList/backend/settings.py
+++ ToDoList/backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'api.apps.ApiConfig',
     'accounts.apps.AccountsConfig',
     
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
